8a4256892ccdc67240907c2f60f66df0defcd35e
Kokate <priti.kokate@capgemini.com>
2017-12-26 15:48:41 +0530

    Hello World Example

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'my-app',
+    template:`
+        <div><h2>Hello World!</h2></div>
+    `
+})
+export class AppComponent {
+
+}
