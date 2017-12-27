f5b3339f14c3697733316fc730322994548c7cca
Kokate <priti.kokate@capgemini.com>
2017-12-27 15:42:34 +0530

    services

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import{FormsModule} from "@angular/forms";
 import{AppComponent} from "./app.component";
 import{ProductComponent} from "../app/Products/products.component";
+import {ProductService} from "../app/Products/product.service";
 
 @NgModule ({
     imports :[BrowserModule, FormsModule],
     declarations :[AppComponent,ProductComponent],
     bootstrap:[AppComponent],
+    providers:[ProductService],
 
 })
 export class AppModule {
