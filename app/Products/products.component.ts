183a11dd56433b03eb83b57db05075f9c465f43a
Kokate <priti.kokate@capgemini.com>
2017-12-27 15:03:59 +0530

    service

--- app/Products/products.component.ts
+++ app/Products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-
+import {IProduct} from "./product";
+import {ProductService} from "./product.service"
 @Component({
     selector:'product-app',
     templateUrl:'app/Products/products.html',
@@ -8,7 +9,11 @@ import { Component } from "@angular/core";
 })
 export class ProductComponent {
     img:boolean;
-    products: any[] = [
+    imageWidth:number=50;
+    imageMargin:number=4;
+    filteredProduct:IProduct[];
+    _listFilter:string;
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -36,4 +41,21 @@ export class ProductComponent {
         this.img=!this.img;
         console.log(this.img);
     }
+
+get listFilter() : string {
+    return this._listFilter;
+}
+
+set listFilter(value:string){
+    this._listFilter = value;
+    
+    this.filteredProduct= this.performFilter(value);
+}
+
+performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+          product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+          
+}	
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import{FormsModule} from "@angular/forms";
 import{AppComponent} from "./app.component";
 import{ProductComponent} from "../app/Products/products.component";
 
 @NgModule ({
-    imports :[BrowserModule],
+    imports :[BrowserModule, FormsModule],
     declarations :[AppComponent,ProductComponent],
     bootstrap:[AppComponent],
 
